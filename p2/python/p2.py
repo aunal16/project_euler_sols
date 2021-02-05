a5a44e8216cc73c3cddb503fb445efbce01cd9e2
aunal16 <aunal16@ku.edu.tr>
2021-02-05 20:14:14 +0300

    The summation of the first 10 EVEN Fibonacci numbers

--- p2/python/p2.py
+++ p2/python/p2.py
@@ -5,14 +5,12 @@ num_term = 10;      # total number of terms
 
 # MAIN PROGRAM
 if __name__ == "__main__":
-    #print(str(num_prev) + ", " + str(num_next), end = '')
-    summation = num_prev + num_next
+    summation = num_next
 
     for _ in range(3, num_term + 1):
         temp = num_next
         num_next += num_prev
         num_prev = temp
-        #print(", " + str(num_next), end = '')
-        summation += num_next
+        if num_next % 2 == 0: summation += num_next
     
     print(summation)
