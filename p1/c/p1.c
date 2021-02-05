dac41b668a85e6f665645c55954cdb3cdad86db9
aunal16 <aunal16@ku.edu.tr>
2021-02-05 19:24:55 +0300

    C code for the example input - output

---
+++ p1/c/p1.c
@@ -0,0 +1,23 @@
+/* IMPORT LIBRARIES */
+#include <stdio.h>
+
+/* INITIALIZE VARIABLES */
+int num1 = 3;		// smaller num
+int num2 = 5;		// larger num
+int num_final = 10;	// limit num
+
+int main(void)
+{
+	/* Assuming num_final > num1 */
+	int sum = 3;
+	int i;
+	for(i  = 4; i < num_final; i++)
+	{
+		if((i % num1 == 0) || (i % num2 == 0))
+		{
+			sum += i;
+		}
+	}
+	printf("The summation of the integers up to %d are %d\n", num_final, sum);
+}
+
