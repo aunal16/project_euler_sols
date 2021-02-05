7ff3f16cab017c179c9002bd5b445ef58fbfc09e
aunal16 <aunal16@ku.edu.tr>
2021-02-05 19:14:09 +0300

    Python code for testing the example input - output.

---
+++ p1/python/p1.py
@@ -0,0 +1,15 @@
+# INITIALIZATIONS
+num1 = 3        # small num
+num2 = 5        # larger num
+num_end = 10    # limit num
+
+# MAIN PROGRAM
+if __name__ == "__main__":
+    # Assuming num_end > num1
+    summation = 3
+    for i in range(4, num_end):
+        if ((i % num1 == 0) or (i % num2 == 0)):
+            summation += i
+    
+    print("Sum of the multiplies up to", num_end, "is", summation)
+
