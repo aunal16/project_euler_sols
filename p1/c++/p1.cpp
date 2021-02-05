1996da0d34f6ee04bacc1ae0093a8a48a372f38e
aunal16 <aunal16@ku.edu.tr>
2021-02-05 19:38:17 +0300

    C++ code for the example input & outputs

--- p1/c++/p1.cpp
+++ p1/c++/p1.cpp
@@ -1,17 +1,18 @@
 /* IMPORT LIBRARIES */
-#include <stdio.h>
+#include <iostream>
 
 /* INITIALIZE VARIABLES */
 int num1 = 3;		// smaller num
 int num2 = 5;		// larger num
-int num_final = 1000;	// limit num
+int num_final = 10;	// limit num
 
-int main(void)
+/* MAIN PROGRAM */
+int main()
 {
 	/* Assuming num_final > num1 */
-	int sum = 3;
+	int sum = num1;
 	int i;
-	for(i  = 4; i < num_final; i++)
+	for(i = 4; i<num_final; i++)
 	{
 		if((i % num1 == 0) || (i % num2 == 0))
 		{
@@ -21,4 +22,3 @@ int main(void)
 	printf("The summation of the integers up to %d are %d\n", num_final, sum);
 	return 0;
 }
-
