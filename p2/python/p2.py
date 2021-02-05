20ea52e05dd1159f9581de8031daf67ad9bfb4d5
aunal16 <aunal16@ku.edu.tr>
2021-02-05 20:30:27 +0300

    Summation of the even numbers for the limit 89 (10th Fibonacci number)

--- p2/python/p2.py
+++ p2/python/p2.py
@@ -1,16 +1,16 @@
 # INITIALIZE VARIABLES
 num_prev = 1;       # first Fibonacci number
 num_next = 2;       # second Fibonacci number
-num_term = 10;      # total number of terms
+num_lim = 89;       # limit for the largest Fibonacci number
 
 # MAIN PROGRAM
 if __name__ == "__main__":
-    summation = num_next
+    summation = 0
 
-    for _ in range(3, num_term + 1):
+    while num_next <= num_lim:
+        if num_next % 2 == 0: summation += num_next
         temp = num_next
         num_next += num_prev
         num_prev = temp
-        if num_next % 2 == 0: summation += num_next
-    
+        
     print(summation)
