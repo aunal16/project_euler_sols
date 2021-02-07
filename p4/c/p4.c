8d018d16e0e5128854883f7ed9b637468e74a5a8
aunal16 <aunal16@ku.edu.tr>
2021-02-07 19:03:28 +0300

    Final version, tested for digit = 2 and digit = 3

--- p4/c/p4.c
+++ p4/c/p4.c
@@ -9,24 +9,37 @@ int digit = 3;
 
 /* FUNCTION PROTOTYPES */
 int is_palindrome(int);
+int get_max(int*);
 
 /* MAIN PROGRAMS */
 int main(void)
 {
-	int* palindromes =  malloc(sizeof(int));
+	/* Store number of elements in the first element. */
+	int* palindromes =  (int*) malloc(sizeof(int));
+	(*palindromes)++;
+
 	const int n_min = pow(10, digit - 1);
 	const int n_max = pow(10, digit) - 1;
 
 	int i, j;
+	int num_of_elements;
 	for(i = n_min; i <= n_max; i++)
 	{
 		for(j = n_min; j <= n_max; j++)
 		{
 			int multiplication = i * j;
+			if (is_palindrome(multiplication))
+			{
+				num_of_elements = palindromes[0];
+				palindromes = (int*) realloc(palindromes, (num_of_elements + 1) * sizeof(int));
+				(*palindromes)++; // store the increase in the num of elements in the array
+				palindromes[num_of_elements - 1] = multiplication;
+			}
 		}
 	}
-	
-	printf("%d\n", is_palindrome(101010));
+	int max_element = get_max(palindromes);
+	printf("%d\n", max_element);
+
 	return 0;
 }
 
@@ -45,4 +58,20 @@ int is_palindrome(int x)
 	}
 	       
 	return check;
+}
+
+int get_max(int* arr)
+{
+	int num_of_elements = arr[0];
+	int max_num = arr[1];
+	int i;
+	for(i = 2; i < num_of_elements - 1; i ++)
+	{
+		if(arr[i] > max_num)
+		{
+			max_num = arr[i];
+		}
+	}
+
+	return max_num;
 }
