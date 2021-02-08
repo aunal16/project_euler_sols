3e8ddceb63904071db4131a5be120ed46a87eb25
aunal16 <aunal16@ku.edu.tr>
2021-02-08 23:54:04 +0300

    Final solution for the range [1,10]

--- p5/c/p5.c
+++ p5/c/p5.c
@@ -14,16 +14,49 @@ void factorize(int, int*);
 /* MAIN PROGRAM */
 int main(void)
 {
-	
 	int i, j;
 	for(i = 2; i <= limit; i++)
 	{
 		int* factors = (int*) malloc(sizeof(int));
 		factorize(i, factors);
 
+		int count;
+		int elem;
+		j = 1;
+		while(j <= factors[0])
+		{
+			count = 1;			
+			elem = factors[j];
+			while((j < factors[0]))
+			{
+				if(factors[j + 1] == elem)
+				{
+					count++;
+					j++;
+				} else
+				{
+					j++;
+				}
+			}
+			if(count > dividers[elem - 1])
+			{
+				dividers[elem - 1] = count;
+			}
+			if(j == factors[0]) {j++;}
+		}
 		free(factors);
 	}
 
+	int number = 1;
+	int exponent;
+	for(i = 0; i < limit; i++)
+	{
+		exponent = dividers[i];
+		if ( exponent != 0)
+			number *= pow(i + 1, exponent);
+	}
+
+	printf("Smallest number that can be divided for all [1, %d] is %d\n", limit, number);
 	return 0;
 }
 
@@ -50,7 +83,34 @@ int is_prime(int x)
 
 void factorize(int x, int* arr)
 {
-	if (x == 1)
-		return NULL;
 	if ((x == 2) || (x == 3))
+	{
+		arr[0]++;
+		arr = (int*) realloc(arr, (arr[0] + 1) * sizeof(int));
+		arr[arr[0]] = x;
+	} else
+	{
+		int i;
+		int sqrt_x = sqrt(x);
+
+		for (i = 2; i <= sqrt_x; i++)
+		{
+			while(x % i == 0)
+			{
+				arr[0]++;
+				arr = (int*) realloc(arr, (arr[0] + 1) * sizeof(int));
+				arr[arr[0]] = i;
+				x /= i;
+			}
+		}
+
+		if (is_prime(x))
+		{
+			arr[0]++;
+			arr = (int*) realloc(arr, (arr[0] + 1) * sizeof(int));
+			arr[arr[0]] = x;
+		}
+	}
+}
+
 
