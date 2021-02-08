5c9da2eb74ac9c349808deb269c57f8fb6e71baa
aunal16 <aunal16@ku.edu.tr>
2021-02-08 21:50:25 +0300

    Final comments for readability

--- p5/python/p5.py
+++ p5/python/p5.py
@@ -1,9 +1,17 @@
+# # IMPORT LIBRARIES
 import numpy as np
 
+# # GLOBAL VARIABLES
+# This sets the range from 1 to limit (including)
 limit = 20
+# This holds the (max) number of the dividers of the numbers in the range
 dividers = np.zeros(limit)
 
+# # FUNCTIONS
 def is_prime(x):
+    """
+    This function checks whether x is prime or not and returns True-False
+    """
     if (x == 1):                            return False
     if (x == 2) or (x == 3) or (x == 5):    return True
     if (x % 2 == 0):                        return False
@@ -16,12 +24,17 @@ def is_prime(x):
     return check
 
 def factorize(x):
-    if (x == 1): return []
+    """ 
+    This function decomposes x into its prime dividers and returns these
+    dividers in an array.
+    """
+    if (x == 1): return [] # 1 is not prime
     if (x == 2) or (x == 3): return [x]
 
     factors = np.array([])
     i = 2
     while i < (int(np.sqrt(x)) + 1):
+        # it is enough to check until the square root of x
         if (x % i == 0):
             if is_prime(i):
                 factors = np.append(factors, i)
@@ -35,7 +48,7 @@ def factorize(x):
     if is_prime(x): factors = np.append(factors, x)
     return factors.astype(int)
 
-
+# # MAIN PROGRAM
 if __name__ == "__main__":
     for i in range(2, limit + 1):
         factors = factorize(i)
