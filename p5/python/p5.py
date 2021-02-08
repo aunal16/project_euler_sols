e45dada3578b760a54d04964b953b537aa0b9c0a
aunal16 <aunal16@ku.edu.tr>
2021-02-08 21:40:19 +0300

    Working solution for 2520

--- p5/python/p5.py
+++ p5/python/p5.py
@@ -15,3 +15,38 @@ def is_prime(x):
     
     return check
 
+def factorize(x):
+    if (x == 1): return []
+    if (x == 2) or (x == 3): return [x]
+
+    factors = np.array([])
+    i = 2
+    while i < (int(np.sqrt(x)) + 1):
+        if (x % i == 0):
+            if is_prime(i):
+                factors = np.append(factors, i)
+                factors = np.append(factors, factorize(x / i))
+                break
+            else:
+                factorize(i)
+        else:
+            i += 1
+    
+    if is_prime(x): factors = np.append(factors, x)
+    return factors.astype(int)
+
+
+if __name__ == "__main__":
+    for i in range(2, limit + 1):
+        factors = factorize(i)
+        elems, counts = np.unique(factors, return_counts = True)
+        for j in range(len(elems)):
+            if (counts[j] > dividers[elems[j] - 1]):
+                dividers[elems[j]  - 1] = counts[j]
+
+    number = 1
+    for i in range(limit):
+        exponent = dividers[i]
+        if exponent != 0: number *= (i+1)**exponent
+    print("Smallest number that can be divided for all [1," + str(limit) +"] is:", int(number))
+
