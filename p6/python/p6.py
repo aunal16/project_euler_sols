c02562fc7b69d82b754f0a7a481c46ec0d3299d5
aunal16 <aunal16@ku.edu.tr>
2021-02-09 14:02:31 +0300

    Final solution for limit = 10

---
+++ p6/python/p6.py
@@ -0,0 +1,12 @@
+# GLOBAL VARIABLES
+limit = 10
+
+# MAIN PROGRAM
+if __name__ == "__main__":
+    sum = 0
+    for i in range(1, limit):
+        for j in range(i + 1, limit + 1):
+            sum += i * j
+
+    sum *= 2
+    print(sum)
