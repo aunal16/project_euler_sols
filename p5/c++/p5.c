91e5e5010d32017e67c37671f8d48cbeee2097b3
aunal16 <aunal16@ku.edu.tr>
2021-02-09 14:42:31 +0300

    factorize is implemented and tested for [2,20)

--- p5/c++/p5.c
+++ p5/c++/p5.c
@@ -9,10 +9,17 @@ int dividers[LIMIT];
 
 /* FUNCTION PROTOTYPES */
 int is_prime(int);
-std::vector<int> factorize(int, int*);
+std::vector<int> factorize(int);
 
 int main(void)
 {
+	for(int k = 2; k < 20; k++)
+	{
+		std::vector<int> arr = factorize(k);
+		for(int i = 0; i < arr.size(); i++)
+			printf("%d\t", arr.at(i));
+		printf("\n");
+	}
 
 	return 0;
 }
@@ -35,4 +42,29 @@ int is_prime(int x)
 	}
 
 	return check;
+}
+
+std::vector<int> factorize(int x)
+{
+	std::vector<int> array(0);
+	if((x == 2) || (x == 3))
+	{
+		array.push_back(x);
+	} else
+	{
+		int i;
+		for(i = 2; i <= sqrt(x); i++)
+		{
+			while(x % i == 0)
+			{
+				array.push_back(i);
+				x /= i;
+			}
+		}
+
+		if (is_prime(x))
+			array.push_back(x);
+	}
+
+	return array;
 }
