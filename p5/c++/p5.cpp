527f3ac0c68a0c8af0220ea3607c36750067696c
aunal16 <aunal16@ku.edu.tr>
2021-02-09 15:13:06 +0300

    Final solution for LIMIT 20

--- p5/c++/p5.cpp
+++ p5/c++/p5.cpp
@@ -1,7 +1,9 @@
 /* IMPORT LIBRARIES */
-#include <iostream>
-#include <math.h>
-#include <vector>
+#include <iostream>	//for printf
+#include <math.h>	//for sqrt, pow
+#include <vector>	//for vector
+#include <set>		//for set
+#include <algorithm>	//for count
 
 /* GLOBAL VARIABLES */
 #define LIMIT 20
@@ -13,13 +15,31 @@ std::vector<int> factorize(int);
 
 int main(void)
 {
-	for(int k = 2; k < 20; k++)
+	int i;
+	for(i = 2; i <= LIMIT; i ++)
 	{
-		std::vector<int> arr = factorize(k);
-		for(int i = 0; i < arr.size(); i++)
-			printf("%d\t", arr.at(i));
-		printf("\n");
+		std::vector<int> factors = factorize(i);
+		std::set<int> s(factors.begin(), factors.end());
+		int count;
+
+		for(int elem: s)
+		{
+		
+			count = std::count(factors.begin(), factors.end(), elem);
+			if(dividers[elem - 1] < count)
+				dividers[elem - 1] = count;
+		}
 	}
+
+	int number = 1;
+	int exponent;
+	for(i = 0; i < LIMIT; i++)
+	{
+		exponent = dividers[i];
+		if(exponent != 0)
+			number *= pow(i + 1, exponent);
+	}
+	printf("Smallest number that can be divided for all [1, %d] is %d\n", LIMIT, number);
 
 	return 0;
 }
