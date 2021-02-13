c0d30a3af88f2474627f6022d0fcd1b55f0d86d7
aunal16 <aunal16@ku.edu.tr>
2021-02-13 14:49:25 +0300

    final solution for 6th prime: 13

---
+++ p7/c/p7.c
@@ -0,0 +1,43 @@
+/* IMPORT LIBRARIES */
+#include <stdio.h>	// for printf
+#include <math.h>	// for sqrt
+
+/* GLOBAL VARIABLES */
+#define N 6
+
+/* FUNCTION DECLARATIONS */
+int is_prime(int);
+
+/* MAIN PROGRAM */
+int main(void)
+{
+	int count = 0;
+	int num = 1;
+
+	while(count < N)
+	{
+		if(is_prime(num))
+			count++;
+		num++;
+	}
+
+	printf("%dth prime is %d\n", N, num - 1);
+}
+
+int is_prime(int x)
+{
+	if(x == 1)
+		return 0;
+	if(x == 2)
+		return 1;
+	if(x % 2 == 0)
+		return 0;
+
+	int check = 1;
+	int i;
+	for(i = 3; i <= sqrt(x); i+= 2)
+		if(x % i == 0)
+			check = 0;
+
+	return check;
+}
