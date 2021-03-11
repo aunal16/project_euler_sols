5190e876c9ba45f5f0e377f0604436c96756c360
aunal16 <aunal16@ku.edu.tr>
2021-03-11 14:28:14 +0300

    converted lines (Strings) to integer arrays

--- p11/python/p11.py
+++ p11/python/p11.py
@@ -1,6 +1,16 @@
+# IMPORT LIBRARIES
+import numpy as np
+
 # Open the grid.txt file for reading
 f = open("../grid.txt", "r")
 
 if __name__ == "__main__":
-    print(f.readline())
+    lines = f.readlines()
+
+    grid = np.array([])
+
+    for line in lines:
+        line_list       = line.split()
+        line_int_list   = list(map(int, line_list))
+        print(line_int_list)
     f.close()
