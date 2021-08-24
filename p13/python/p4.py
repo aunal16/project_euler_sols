583233377f7ce532c1d08eb817998682a952d780
aunal16 <aunal16@ku.edu.tr>
2021-08-24 20:51:21 +0300

    draft is created, ready for initial test for reading numbers from txt

---
+++ p13/python/p4.py
@@ -0,0 +1,26 @@
+## IMPORT LIBRARIES
+import numpy as np
+
+## VARIABLES
+LEN_NUM = 50
+
+## OPEN, READ, CLOSE TXT FILE (SAVE NUMBERS)
+#  Open the txt file for reading and read lines
+fin = open("../numbers.txt", "r")
+lines = f.readlines()
+
+#Create an array to hold the numbers and digits
+arr_size = len(lines)
+arr_nums = np.empty([arr_size, 1])
+arr_digits = np.empty([arr_size, LEN_NUM])
+
+# Convert strings of the input file into integers
+# and store them in array_nums. Furher, arr_digit
+for i in range(arr_size):
+    arr_nums[i] = list(map(int, lines[i]))
+
+## FUNCTIONS
+
+## MAIN PROGRAM
+if __name__ == "__main__":
+    print(arr_nums)
