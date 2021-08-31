cf736229151ae6f26df54f8f27c9b11efe343fbb
aunal16 <aunal16@ku.edu.tr>
2021-09-01 00:39:16 +0300

    inefficient method that checks all numbers from scratch. cannot compute

--- p14/python/p14.py
+++ p14/python/p14.py
@@ -1,7 +1,7 @@
 ## IMPORT LIBRARIES
 
 ## VARIABLES
-LIMIT = 15
+LIMIT = 1e6
 
 ## FUNCTIONS
 def is_even(x):
@@ -18,14 +18,13 @@ if __name__ == "__main__":
     # Set the initial starting number
     N = 2
     max_n_terms = 1
+    max_n       = N
 
     while N < LIMIT:
 
         n = N
         # Set the initial number of terms
         n_terms = 1
-
-        print(n, " -> ", end = "")
 
         while n != 1:
             # Generate the next number in
@@ -36,22 +35,11 @@ if __name__ == "__main__":
                 n = 3 * n + 1
 
             n_terms += 1
-            
-            # Print the sequence to check
-            print(n, " -> ", end = "")
 
         if max_n_terms < n_terms:
             max_n_terms = n_terms
+            max_n       = N
 
         # Increase starting number N by 1
         N += 1
-
-        print(" /// n_terms: ", n_terms)
-
-    print(" /// max_n_terms: ", max_n_terms)
-
-
-
-
-
 
